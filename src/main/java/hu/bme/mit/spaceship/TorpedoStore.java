76236a62ccf9f48d41215f5f72608e2d42be9d8e
Péter Fógel <fogel_peter@sch.bme.hu>
2020-05-02 17:04:50 +0200

    random_fix

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -28,6 +28,8 @@ public class TorpedoStore {
     }
   }
 
+  // simulate random overheating of the launcher bay which prevents firing
+  private Random generator = new Random();
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
@@ -35,8 +37,7 @@ public class TorpedoStore {
 
     boolean success = false;
 
-    // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
+    
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
